36b14156ec3842e3ff316db29fecd4dd8972a93e
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2024-08-08 10:37:05 +0900

    Case insensitivity

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
-use std::{error::Error, fs};
+use std::{env, error::Error, fs};
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -13,13 +14,24 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+    for line in results {
         println!("{line}");
     }
 
@@ -30,6 +42,18 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
     for line in contents.lines() {
         if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
             results.push(line);
         }
     }
@@ -72,5 +96,46 @@ Pick three.";
         let contents = fs::read_to_string("poem.txt").unwrap();
 
         assert_eq!(vec![] as Vec<&str>, search(query, &contents));
+    }
+
+    #[test]
+    fn one_result_case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+
+    #[test]
+    fn multiple_results_case_insensitive() {
+        let query = "BOdy";
+        let contents = fs::read_to_string("poem.txt").unwrap();
+
+        assert_eq!(
+            vec![
+                "I'm nobody! Who are you?",
+                "Are you nobody, too?",
+                "How dreary to be somebody!"
+            ],
+            search_case_insensitive(query, &contents)
+        );
+    }
+
+    #[test]
+    fn no_results_case_insensitive() {
+        let query = "metAMorphosis";
+        let contents = fs::read_to_string("poem.txt").unwrap();
+
+        assert_eq!(
+            vec![] as Vec<&str>,
+            search_case_insensitive(query, &contents)
+        );
     }
 }
