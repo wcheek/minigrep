e06061673b0b04ed4abfdd2965d2f530cf41154d
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2024-08-08 15:44:42 +0900

    Iterators implementation

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,8 @@ use minigrep::Config;
 use std::{env, process};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
@@ -13,7 +11,7 @@ fn main() {
     println!("In file {}", config.file_path);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
